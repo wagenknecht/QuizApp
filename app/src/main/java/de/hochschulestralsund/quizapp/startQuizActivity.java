d04ed867f1d04b5c37e2cd6cecf8b127eabcdf8f
wagenknecht <70338036+FWDL@users.noreply.github.com>
2022-06-10 15:43:23 +0200

    dummy menu for starting a quiz

--- app/src/main/java/de/hochschulestralsund/quizapp/startQuizActivity.java
+++ app/src/main/java/de/hochschulestralsund/quizapp/startQuizActivity.java
@@ -3,12 +3,40 @@ package de.hochschulestralsund.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class startQuizActivity extends AppCompatActivity {
+public class startQuizActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_quiz);
+
+        Spinner selectCategorySpinner = findViewById(R.id.selectCategorySpinner);
+        ArrayAdapter<CharSequence> selectCategorySpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.categories, android.R.layout.simple_spinner_item);
+        selectCategorySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectCategorySpinner.setAdapter(selectCategorySpinnerAdapter);
+        selectCategorySpinner.setOnItemSelectedListener(this);
+
+        Spinner selectDifficultySpinner = findViewById(R.id.selectDifficultySpinner);
+        ArrayAdapter<CharSequence> selectDifficultyAdapter = ArrayAdapter.createFromResource(this,
+                R.array.difficulties, android.R.layout.simple_spinner_item);
+        selectDifficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectDifficultySpinner.setAdapter(selectDifficultyAdapter);
+        selectDifficultySpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
